4a2a7ec74ce66fbc4fec14aa46cde24a8f708396
Zach Keeshin <zachkeeshin@Zachs-MacBook-Air.local>
2017-04-14 15:55:25 -0500

    test

--- ClozeCard.js
+++ ClozeCard.js
@@ -3,6 +3,7 @@
 var ClozeCard = function(fullText, clozeDeletion) {
 	this.fullText = fullText;
 	this.clozeDeletion = clozeDeletion;
+	this.clozeIndex = this.fullText.indexOf(clozeDeletion);
 }
 
 module.exports = ClozeCard;
