ee7dbd1feab5f807854a564c7a99c26124a032d5
Zach Keeshin <zachkeeshin@Zachs-MacBook-Air.local>
2017-04-13 23:33:51 -0500

    test

--- BasicCard.js
+++ BasicCard.js
@@ -1,16 +1,8 @@
 
 
-function BasicCard(frontSide, backSide) {
+var BasicCard = function(frontSide, backSide) {
 	this.frontSide = frontSide;
 	this.backSide = backSide;
 }
-
-var firstPresident = new BasicCard(
-		"Who was the first president of the United States?", 
-		"George Washington"
-	)
-
-console.log(firstPresident.frontSide); 
-console.log(firstPresident.backSide); 
 
 module.exports = BasicCard;
